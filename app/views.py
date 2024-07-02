85f62402582c72860e6931768b1d8e32d8714c86
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-07-02 22:55:00 +0100

    used new package to get the users ipaddress

--- app/views.py
+++ app/views.py
@@ -11,49 +11,56 @@ from django.conf import settings
 def simpleserver(request: HttpRequest):
 
     
-    response = requests.get('https://api.ipify.org?format=json')
-    if response.status_code == 200:
-        ip_data = response.json()
-    ip  = ip_data['ip']
-
+    # r = requests.get(r'http://jsonip.com')
+    # ip= r.json()['ip']
+    # print ('Your IP is', ip)
     # get the client name
-    client_name = request.GET.get("visitor_name", "Guest")
 
-    # access users ip local ip_address, if application is behin d a proxy, or loadbalancer.
-    # client_ip = get_client_ip(request)[0]
+    client_ip, is_routable = get_client_ip(request)
 
-    # get the client public ip address, using geojs api, when i tried getting the users information using the local ipaddress, geojs api returned some invalid response, so i had to use the public ip address.
-    # client_public_ip = requests.get(f"https://get.geojs.io/v1/ip.json")
+    if client_ip is None or client_ip == '127.0.0.1':
+        response = requests.get('https://api.ipify.org?format=json')
+        if response.status_code == 200:
+            client_ip = response.json().get('ip')
 
-    response = requests.get(
-        f"https://get.geojs.io/v1/ip/geo/{ip}.json"
-    )
 
-    # print(response)
+        client_name = request.GET.get("visitor_name", "Guest")
 
-    client_city = response.json()["city"]
+        # access users ip local ip_address, if application is behin d a proxy, or loadbalancer.
+        client_ip = get_client_ip(request)[0]
 
-    # openwhethermap api conf.
-    # get client's city whether information is available using openwhois api
-    weather_api_key = settings.OPENWEATHER_API_KEY
+        # get the client public ip address, using geojs api, when i tried getting the users information using the local ipaddress, geojs api returned some invalid response, so i had to use the public ip address.
+        client_public_ip = requests.get(f"https://get.geojs.io/v1/ip.json")
 
-    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-    CITY = client_city
-    API_KEY = weather_api_key
+        response = requests.get(
+            f"https://get.geojs.io/v1/ip/geo/{client_public_ip.json()['ip']}.json"
+        )
 
-    whether_api_url = f"{BASE_URL}q={CITY}&appid={API_KEY}"
+        # print(response)
 
-    weather_response = requests.get(whether_api_url).json()
+        client_city = response.json()["city"]
 
-    client_city_tmp = weather_response["main"]["temp"]
-    print(client_city_tmp)
+        # openwhethermap api conf.
+        # get client's city whether information is available using openwhois api
+        weather_api_key = settings.OPENWEATHER_API_KEY
 
-    # return a response.
-    print()
-    return JsonResponse(
-        {
-            "client_ip": ip,
-            "location": client_city,
-            "greeting": f"Hello, {client_name}!, the temperature is {str(client_city_tmp)} degrees Celcius in {client_city}",
-        }, json_dumps_params={"indent": 4}
-    )
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        CITY = client_city
+        API_KEY = weather_api_key
+
+        whether_api_url = f"{BASE_URL}q={CITY}&appid={API_KEY}"
+
+        weather_response = requests.get(whether_api_url).json()
+
+        client_city_tmp = weather_response["main"]["temp"]
+        print(client_city_tmp)
+
+        # return a response.
+        print()
+        return JsonResponse(
+            {
+                "client_ip": str(client_public_ip.json()["ip"]),
+                "location": client_city,
+                "greeting": f"Hello, {client_name}!, the temperature is {str(client_city_tmp)} degrees Celcius in {client_city}",
+            }, json_dumps_params={"indent": 4}
+        )
